6e28d68f72c99a760511b30667b6e955a7319f02
dib-a <kirakirito62@gmail.com>
2022-03-28 07:06:59 +0200

    added suckless st dmenu

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 mkdir suckless
 cd suckless
 git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/dmeu
+git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
 cd st
 sudo make clean install
 cd ../dmenu
 sudo make clean install
-cd ..
-sh ../update.sh
+cd ../../
+sudo chmod -R 755 suckless
+sh update.sh
