c33b7badc5d8e4c6a6db2db46291b4b20064d5e3
dib-a <kirakirito62@gmail.com>
2022-03-28 07:28:39 +0200

    changed colors

--- variables.h
+++ variables.h
@@ -1,5 +1,9 @@
-static const char col_bar[] 		= "#000000";
-static const char col_border[]		= "#000000";
-static const char col_font[] 		= "#26a269";
-static const char col_font_selected[]	= "#000000";
-static const char col_bg_selected[]	= "#26a269";
+/*
+ * #2ec27e	green
+ * #241f31	gray
+ */
+static const char col_bar[] 		= "#241f31";
+static const char col_border[]		= "#241f31";
+static const char col_font[] 		= "#2ec27e";
+static const char col_font_selected[]	= "#241f31";
+static const char col_bg_selected[]	= "#2ec27e";
