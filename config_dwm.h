7a03e9bccc7c66c69c3dc020ccf157dcf3a72639
dib-a <kirakirito62@gmail.com>
2022-03-28 08:50:05 +0200

    changed fontsize

--- config_dwm.h
+++ config_dwm.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const char *fonts[]          = { "Source Code Pro:size=11" };
+static const char dmenufont[]       = "Source Code Pro:size=11";
 static const char *colors[][3]      = {
 	/*               fg			bg         		border   */
 	[SchemeNorm] = { col_font,		col_bar, 		col_border},
