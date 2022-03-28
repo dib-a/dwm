82564158a26c96f991b908a5d825c6e518d5a305
dib-a <kirakirito62@gmail.com>
2022-03-28 08:44:11 +0200

    split update

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
+MY_PATH=$(cd "$MY_PATH" && pwd)
+
 mkdir suckless
 cd suckless
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
-cd st
+cd dmenu
 sudo make clean install
-cd ../dmenu
-sudo make clean install
-cd ../../
+cd $MY_PATH
 sudo chmod -R 755 suckless
-sh update.sh
+sh update_dwm.sh
+sh update_st.sh
