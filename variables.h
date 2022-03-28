1acc510af39a992e27525d12c5ffdb851344cd62
dib-a <kirakirito62@gmail.com>
2022-03-28 17:17:14 +0200

    added font_variable

--- variables.h
+++ variables.h
@@ -1,9 +1,4 @@
-/*
- * #2ec27e	green
- * #241f31	gray
- */
-static const char col_bar[] 		= "#241f31";
-static const char col_border[]		= "#241f31";
-static const char col_font[] 		= "#2ec27e";
-static const char col_font_selected[]	= "#241f31";
-static const char col_bg_selected[]	= "#2ec27e";
+static const char col_green[] = "#2ec27e";
+static const char col_black[] = "#241f31";
+
+static char font_source_code_pro[] = "Source Code Pro:size=11";
