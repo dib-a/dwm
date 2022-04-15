480e4761ad4cc26024499696814d139a327f970f
dib-a <96333029+dib-a@users.noreply.github.com>
2022-04-15 08:29:15 +0200

    added some useful software and fonts

---
+++ useful.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo pacman -S inkscape gimp okular ranger neofetch htop geogebra 
+
+cd opt
+mkdir fonts
+cd fonts
+
+git clone https://aur.archlinux.org/ttf-qurancomplex-fonts.git
+cd ttf-qurancomplex-fonts
+makepkg -si
+
+cd /opt/fonts
+
+git clone https://aur.archlinux.org/ttf-poppins.git
+cd ttf-poppins
+makepkg -si
