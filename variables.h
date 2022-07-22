f04272cf9f8b058dd2cd893245feaa5cf3a3c9d4
dib-a <kirakirito62@gmail.com>
2022-07-22 13:44:38 +0200

    fixed fonts

--- variables.h
+++ variables.h
@@ -1,4 +1,4 @@
 static const char col_fg[] = "#ffffff";
 static const char col_bg[] = "#000000";
 
-static char font[] = "IBM Plex Sans Arabic:size=11";
+static char var_font[] = "IBM Plex Mono:pixelsize=15:antialias=true:autohint=true";
