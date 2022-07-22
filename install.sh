1b77c9ba1f65cffbdaec88c9d103f0a63b0f9e82
dib-a <96333029+dib-a@users.noreply.github.com>
2022-07-22 11:18:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
-path=$(cd pwd)
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxinerama ttf-ibm-plex ttf-font-awesome feh
 
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxinerama ttf-ibm-plex feh
-
-cd src
-mkdir suckless
-cd suckless
 git clone https://git.suckless.org/dwm
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/st
+
 cd dmenu
 sudo make clean install
-$path
-cd src
-sudo chmod -R 755 suckless
-cd scripts
-sh all.sh
+cd ../dwm
+sudo make clean install
+cd ../st
+sudo make clean install
