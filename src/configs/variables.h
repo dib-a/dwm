9d8675b52c72e5092506cc85aa7f5ca17b58c724
dib-a <96333029+dib-a@users.noreply.github.com>
2022-07-22 11:30:56 +0200

    changed color black

--- src/configs/variables.h
+++ src/configs/variables.h
@@ -1,4 +1,4 @@
 static const char col_green[] = "#2ec27e";
-static const char col_black[] = "#241f31";
+static const char col_black[] = "#000000";
 
 static char font_source_code_pro[] = "Source Code Pro:size=11";
