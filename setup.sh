ae79724fdbc1deea3eccae79b0fbfb1f53a33e33
dib-a <96333029+dib-a@users.noreply.github.com>
2023-02-10 04:33:03 +0000

    added make

--- setup.sh
+++ setup.sh
@@ -5,10 +5,18 @@ sudo pacman -S libx11 libxft xorg-server xorg-xinit xorg-xsetroot ttf-ibm-plex
 mkdir ~/.build
 cd ~/.build
 
-git clone git://git.suckless.org/st
-git clone git://git.suckless.org/dmenu
 git clone git://git.suckless.org/dwm
-
+git clone git://git.suckless.org/dmenu
+git clone git://git.suckless.org/st
 git clone https://github.com/dib-a/dwm-config
+
+cd dwm
+sudo make clean install
+
+cd ../dmenu
+sudo make clean install
+
+cd ../st
+sudo make clean install
 
 sh update-configs.sh
