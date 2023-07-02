88f7ac9025d9d999eba64ab7d2d402e4fbc08ccf
dib-a <dib.ahmad.000@gmail.com>
2023-07-02 14:51:56 +0200

    split into functions

--- setup.sh
+++ setup.sh
@@ -5,27 +5,43 @@ sudo pacman -S libx11 libxft libxinerama xorg-server xorg-xinit xorg-xsetroot tt
 mkdir -p /opt/dwm
 cd /opt/dwm
 
-git clone git://git.suckless.org/dwm
-git clone git://git.suckless.org/dmenu
-# git clone git://git.suckless.org/st
+suckless() {
+    git clone git://git.suckless.org/dwm
+    git clone git://git.suckless.org/dmenu
+    # git clone git://git.suckless.org/st
 
-cd dwm
-sudo make clean install
+    cd dwm
+    sudo make clean install
 
-cd ../dmenu
-sudo make clean install
+    cd ../dmenu
+    sudo make clean install
 
-# cd ../st
-# sudo make clean install
+    # cd ../st
+    # sudo make clean install
 
-read -p "Do you want to add dib-a customizations? (y/n): " choice
+}
 
-if [ "$choice" == "y" ]; then
-    echo "Adding customizations..."
-    sh /opt/dwm/update.sh
-else
-    echo "Skipping customization."
-fi
+directories() {
+    sh /opt/dwm/directories.sh
+    cp /opt/dwm/img/tree.jpg /home/$USER/media/wallpapers/tree.jpg
+}
 
-rm /home/$USER/.xinitrc
-cp /opt/dwm/xinitrc /home/$USER/.xinitrc
+customization() {
+    read -p "Do you want to add dib-a customizations? (y/n): " choice
+
+    if [ "$choice" == "y" ]; then
+        echo "Adding customizations..."
+        sh /opt/dwm/update.sh
+        cp /opt/dwm/img/tree.jpg /home/$USER/media/wallpapers/tree.jpg
+    else
+        echo "Skipping customization."
+    fi
+
+    rm /home/$USER/.xinitrc
+    cp /opt/dwm/xinitrc /home/$USER/.xinitrc
+
+    directories
+}
+
+suckless
+customization
