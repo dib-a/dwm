5221d4bfd9b8df58c0d06c0070f8e355005680b9
dib-a <dib.ahmad.000@gmail.com>
2023-07-03 12:22:49 +0200

    fixed path

--- update.sh
+++ update.sh
@@ -5,25 +5,29 @@ cd /opt/dwm
 remove_if_exists() {
     local path="$1"
     if [ -f "$path" ]; then
-        rm "$path"
+        sudo rm "$path"
     fi
 
     if [ -d "$path" ]; then
-        rm -r "$path"
+        sudo rm -r "$path"
     fi
 }
 
-sudo remove_if_exists "dwm/config.h"
-sudo remove_if_exists "dwm/variables.h"
+remove_if_exists "dwm/config.h"
+remove_if_exists "dwm/variables.h"
 #sudo rm st/config.h
 #sudo rm st/variables.h
 
-cp dwm.h dwm/config.h
+cp /opt/dwm/dwm.h /opt/dwm/dwm/config.h
 #cp st.h st/config.h
-cp variables.h dwm/variables.h
+cp /opt/dwm/variables.h /opt/dwm/dwm/variables.h
 #cp variables.h st/variables.h
 
-cd dwm
+cd /opt/dwm/dwm
 sudo make clean install
+
+cd /opt/dwm/dmenu
+sudo make clean install
+
 #cd ../st
 #sudo make clean install
