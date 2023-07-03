3a63b29e6e78616fd47ddce2ebf631947807f12c
dib-a <dib.ahmad.000@gmail.com>
2023-07-03 11:10:52 +0200

    only remove if exists

--- update.sh
+++ update.sh
@@ -2,8 +2,19 @@
 
 cd /opt/dwm
 
-sudo rm dwm/config.h
-sudo rm dwm/variables.h
+remove_if_exists() {
+    local path="$1"
+    if [ -f "$path" ]; then
+        rm "$path"
+    fi
+
+    if [ -d "$path" ]; then
+        rm -r "$path"
+    fi
+}
+
+sudo remove_if_exists "dwm/config.h"
+sudo remove_if_exists "dwm/variables.h"
 #sudo rm st/config.h
 #sudo rm st/variables.h
 
